e5ee0488d29a99ee01eeb96d74ec61d13c50fca9
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-09-23 07:53:25 -0300

    desafio 35 resolvido

--- exerciciosFundamentos.js
+++ exerciciosFundamentos.js
@@ -841,17 +841,18 @@ const verSeInclui = (str_1, str_2) => {
 console.log(verSeInclui('Adenilson ', ' enilson'))
 
 
-// console.log(['a', 'b', 1] == ['a', 'b', 1]) // são referências diferentes
-// console.log('adenilson'.length)
-
-
-
-
 
 // 35) Crie dois vetores chamados vetorPilha e vetorAdiciona. Inicialmente, o vetorPilha conterá cinco elementos
 // inteiros: [1, 2, 3, 4, 5]. Você deverá adicionar os valores contidos no vetorAdiciona [6, 7, 8, 9, 10] ao vetor pilha
 // e mostrá-los no console. É importante lembrar que o método Push retorna somente o tamanho do Vetor. Ao
 // final das operações imprima os vetores no console.
+
+
+const vetorPilha = [1, 2, 3, 4, 5]
+const vetorAdiciona = [6, 7, 8, 9, 10]
+
+console.log(vetorPilha.push(...vetorAdiciona))
+console.log(vetorPilha)
 
 
 
